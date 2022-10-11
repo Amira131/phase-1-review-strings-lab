b329ad3c3766a7347dc2c2d26bc5e078c6a5f9dd
Amira de Moya <demoyaamira@gmail.com>
2022-10-10 22:03:51 -0500

    commit tree 789180f82e59ae6a4b41b2df335adcb49868f99b

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const currentUser = "Amira de Moya"
-
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
-
+const currentUser = "Amira de Moya";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-
-const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
